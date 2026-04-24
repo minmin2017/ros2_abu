217355b8511debd19a7f4be70de0ff3f130d4991
min <wichayapatmeen@gmail.com>
2026-04-25 01:02:04 +0700

    claude docking fix

---
+++ roboarm_ws/build/my_vision_system/share/my_vision_system/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX//home/minmin/roboarm_ws/build/my_vision_system"
---
+++ roboarm_ws/build/my_vision_system/share/my_vision_system/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/minmin/roboarm_ws/build/my_vision_system"
